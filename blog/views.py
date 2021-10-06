ea9cb155e1ab1c37e7a9202e0e356e501374abba
prkapadnis <prkapadnis2001@gmail.com>
2021-10-06 19:11:51 +0530

    Created Post in database and added 2 posts

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 
 def home(request):
-    context = {}
+    posts = Post.objects.all()
+    context = {'posts': posts}
     return render(request, "blog/home.html", context)
 
 
