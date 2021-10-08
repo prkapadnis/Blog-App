cc8850f9fa5a115c98a35e485283dc401936c46e
prkapadnis <prkapadnis2001@gmail.com>
2021-10-08 18:17:52 +0530

    Create User Profile feature and User profile page

--- main_configuration/settings.py
+++ main_configuration/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 STATIC_URL = '/static/'
 
 STATICFILE_DIRS = [
--- users/models.py
+++ users/models.py
@@ -4,7 +4,15 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='defualt.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f"{self.user.username} profile"
+
+
+class Bio(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=200)
+
+    def __str__(self):
+        return f"{self.user.username} Bio"
